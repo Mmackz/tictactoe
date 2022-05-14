b893e5338f4d9a7df733385f1ee372f788827979
Mmackz <matthewmackenz@gmail.com>
2022-05-13 19:23:43 -0700

    ✨ add basic logic for game

--- index.js
+++ index.js
@@ -1,69 +1,192 @@
-const squares = document.querySelectorAll(".container div");
-const startBtn = document.querySelector("#start");
-const startScreen = document.querySelector(".start-screen");
-
 (function () {
-   let gameActive = false;
-   let round = 0;
-   let currentTurn = null;
-   const player = Player("player", "X");
-   const computer = Player("computer", "O");
+   const squares = document.querySelectorAll(".container div");
+   const startForm = document.querySelector(".form");
+   const startScreen = document.querySelector(".start-screen");
 
-   const GameController = (function () {
-      const selectFirstToGo = () => {
-         const random = Math.floor(Math.random() * 2);
-         return random ? "X" : "O";
-      };
-      const runTurn = (p, i) => {
-         if (gameActive) {
-            const occupiedSquares = player.moves.concat(computer.moves);
-            if (!occupiedSquares.includes(i)) {
-               p.addMove(i);
-               return true;
-            }
-            return false;
-         }
+   const Player = (name, marker, ai = false) => {
+      const moves = [];
+      const addMove = (move) => {
+         moves.push(move);
       };
       return {
-         selectFirstToGo,
-         runTurn
+         name,
+         marker,
+         moves,
+         addMove,
+         ai
       };
+   }
+
+   const GameController = (() => {
+      let gameStarted = false;
+      let aiTurn = false;
+      let currentPlayer = 0;
+      const players = [];
+      const board = new Array(9).fill("");
+
+      const assignPlayers = (player1, player2, ai) => {
+         const bool = Math.random() > 0.5;
+         players.push(Player(player1.value, bool ? "x" : "o"));
+         players.push(Player(player2.value, bool ? "o" : "x", ai));
+      };
+
+      const resetBoard = () => {
+         squares.forEach((square) => square.classList = "");
+      };
+
+      const resetGame = () => {
+         resetBoard();
+         gameStarted = false;
+         players.length = 0;
+         board.fill("");
+      };
+
+      const updateBoard = (index, marker) => {
+         squares[index].classList.add(marker);
+         board[index] = marker;
+      };
+
+      const showBoard = () => {
+         startScreen.classList.add("hide");
+         document.getElementById("main").classList.remove("hide");
+      };
+
+      const changeTurn = () => {
+         currentPlayer = currentPlayer ? 0 : 1;
+      };
+
+      const endGame = (winner) => {
+         resetGame();
+         startScreen.classList.remove("hide");
+         document.getElementById("main").classList.add("hide");
+      };
+
+      const makeAiMove = (marker) => {
+         if (!gameStarted) return;
+         aiTurn = true;
+         setTimeout(() => {
+            const cpuMove = getRandomMove(getFreeSpaces());
+            players[currentPlayer].addMove(cpuMove.toString());
+            updateBoard(cpuMove, marker);
+            aiTurn = false;
+            checkForWinner(players[1]);
+            changeTurn();    
+         }, 1000);
+      };
+
+      const getGameState = () => {
+         return gameStarted;
+      };
+
+      const getFreeSpaces = () => {
+         return board.map((space, index) => {
+            if (space === "") {
+               return index;
+            }
+            return null;
+         }).filter((space) => space !== null);
+      };
+
+      const getRandomMove = (moves) => moves[Math.floor(Math.random() * moves.length)];
+
+      const startGame = (e) => {
+         e.preventDefault();
+         gameStarted = true;
+         resetBoard();
+         showBoard();
+         assignPlayers(e.target.elements[0], e.target.elements[1], e.target.elements[2].checked);
+
+         // set current player based on assignment of x and o
+         currentPlayer = players[0].marker === "x" ? 0 : 1;
+
+         // if cpu is first, set their move
+         if (players[1].ai && players[1].marker === "x") {
+            makeAiMove(players[1].marker);
+         }
+      }
+
+      const checkForWinner = (player) => {
+         // no need to check for winner if less than 5 moves
+         if (board.filter(Boolean).length < 5) return;
+         const winningCombos = [
+            ["0", "1", "2"],
+            ["3", "4", "5"],
+            ["6", "7", "8"],
+            ["0", "3", "6"],
+            ["1", "4", "7"],
+            ["2", "5", "8"],
+            ["0", "4", "8"],
+            ["2", "4", "6"]
+         ];
+
+         for (let i = 0; i < winningCombos.length; i++) {
+            if (winningCombos[i].every((index) => player.moves.includes(index.toString()))) {
+               console.log("winner found");
+               markWinner(winningCombos[i]);
+               gameStarted = false;
+            }
+         }
+      }
+
+      const takeTurn = (index) => {
+         if (board[index] === "") {
+            players[currentPlayer].addMove(index);
+            updateBoard(index, players[currentPlayer].marker);
+            board[index] = players[currentPlayer].marker;
+            changeTurn();
+         }
+      }
+
+      const markWinner = (winningCombo) => {
+         squares.forEach((square, i) => {
+            if (winningCombo.includes(i.toString())) {
+               square.classList.add("winner");
+               setTimeout(() => {
+                  endGame();
+               }, 5000);
+            }
+         })
+      };
+
+      const playRound = (index) => {
+
+         // return if not players turn
+         if (aiTurn) return;
+
+         // return if space selected is not free
+         if (board[index]) return;
+
+         // add player move to board
+         takeTurn(index);
+
+         // check for winner
+         checkForWinner(players[Math.abs(currentPlayer - 1)]);
+
+         if (players[currentPlayer].ai) {
+            makeAiMove(players[currentPlayer].marker);
+         }; 
+      }
+
+      return {
+         getGameState,
+         startGame,
+         playRound
+      }
    })();
 
-   startBtn.onclick = function () {
-      startScreen.classList.add("hide");
-      document.getElementById("main").classList.remove("hide");
-      gameActive = !gameActive;
-      currentTurn = GameController.selectFirstToGo();
-      if (computer.marker === currentTurn) {
-         const randNum = Math.floor(Math.random() * 9);
-         GameController.runTurn(computer, randNum);
-         squares[randNum].classList.add(computer.marker.toLowerCase());
-      }
-      console.log(gameActive);
-   };
+   const { startGame, playRound, getGameState } = GameController;
 
-   squares.forEach((square, i) => {
-      square.onclick = function () {
-         if (GameController.runTurn(player, i)) {
-            this.classList.add(player.marker.toLowerCase());
-         }
-      };
-   });
+   startForm.addEventListener("submit", startGame);
+   squares.forEach((square) => square.addEventListener("click", () => {
+      if (getGameState()) {
+         playRound(square.dataset.index);
+      }   
+   }));
+
 })();
 
-function Player(name, marker) {
-   const moves = [];
-   const addMove = (move) => {
-      moves.push(move);
-   };
-   return {
-      name,
-      marker,
-      moves,
-      addMove
-   };
-}
+
+
 
 /* 
 notes
@@ -77,3 +200,15 @@ notes
   
   left off adding turns for cpu on start (if selected randomly) and allowing player to fill boxes when game started
  */
+
+   //    // winning conditions
+   //    const winningAxes = [
+   //       [0,1,2],
+   //       [3,4,5],
+   //       [6,7,8],
+   //       [0,3,6],
+   //       [1,4,7],
+   //       [2,5,8],
+   //       [0,4,8],
+   //       [2,4,6],
+   //   ];
